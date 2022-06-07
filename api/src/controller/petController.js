a5617c81ee373d4aae82383a81afa2926d72be67
Nicholas <doctorredac@gmail.com>
2022-06-07 15:15:31 -0300

    provinha updated

--- api/src/controller/petController.js
+++ api/src/controller/petController.js
@@ -3,19 +3,19 @@ import {inserir, showing} from "../repository/petRepository.js"
 
 const server = Router();
 
-server.post('/pet', (req, resp) => {
-    const cadastro = req.body.cadastro;
-    const snd = inserir(cadastro);
-    resp.status.send({
-        x: snd
-    });
+server.post('/pet', async (req, resp) => {
+    try {
+        const get = req.body;
+        const snd = await inserir(get); 
+        resp.status(201).send({snd});
+    } catch (err) {
+        resp.status(400).send({erro:err.message});
+    }
 })
-server.get('/novopet', (req, resp) => {
-    const {cadastro} = req.body;
-    const snd = inserir(cadastro);
-    resp.status.send({
-        x: snd
-    });
+
+server.get('/pet', async (req, resp) => {
+    const snd = await showing();
+    resp.send({snd});
 })
 
 export default server;
--- api/src/repository/petRepository.js
+++ api/src/repository/petRepository.js
@@ -1,15 +1,16 @@
 import { con } from "./connection.js"
 
-export async function inserir(inserir) {
+export async function inserir(nomepet) {
     const comandos = `INSERT INTO tb_pet (nm_pet)
                         VALUES (?)`;
-    const [resposta] = con.query(comandos, [inserir]);
+    const [resposta] = await con.query(comandos, [nomepet.nome]);
     return resposta;
 }
 
-export async function showing(info) {
-    const comandos = `SELECT id_pet, nm_pet
+export async function showing() {
+    const comandos = `SELECT id_pet       id,
+                             nm_pet     nome
                       FROM tb_pet`;
-    const [resposta] = con.query(comandos);
+    const [resposta] = await con.query(comandos);
     return resposta;
 }
